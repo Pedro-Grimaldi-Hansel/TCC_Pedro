d85775ad40ca0aaaa33c66b4bf24f63f31a0d0de
Pedro <pedroghansel@gmail.com>
2023-10-18 19:14:00 -0300

    testando hosting

--- dist/next.config.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-/** @type {import('next').NextConfig} */
-var nextConfig = {};
-module.exports = {
-  distDir: 'build'
-};
--- src/app/auth/signUp/dist/page.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var link_1 = require("next/link");
-function SignUp() {
-    return (React.createElement("main", null,
-        React.createElement("h1", null, "Sign Up"),
-        React.createElement(link_1["default"], { href: "/" }, "Home")));
-}
-exports["default"] = SignUp;
